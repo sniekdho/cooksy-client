3e9cc2bd60041a075b6dfadd41e1bb377ad292e7
sniekdhoshafiq <developershafiq9578@gmail.com>
2025-05-23 22:09:29 +0600

    implement handleLike functionality on RecipeDetails page

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { useLoaderData } from "react-router";
+import { AuthContext } from "../contexts/AuthContext";
+import Swal from "sweetalert2";
 
 const RecipeDetails = () => {
   const singleRecipe = useLoaderData();
+  const [recipe, setRecipe] = useState(singleRecipe);
+  const { user } = useContext(AuthContext);
+
   const {
     image,
     title,
@@ -13,7 +18,35 @@ const RecipeDetails = () => {
     instructions,
     categories,
     likeCount,
-  } = singleRecipe || {};
+  } = recipe || {};
+
+  const handleLike = () => {
+    if (user.email === recipe.email) {
+      Swal.fire({
+        icon: "warning",
+        title: "You can't like your own recipe!",
+        timer: 1500,
+        showConfirmButton: false,
+      });
+      return;
+    }
+
+    const newLikes = likeCount + 1;
+
+    fetch(`http://localhost:3000/recipes/${singleRecipe._id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ likeCount: newLikes }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.matchedCount) {
+          setRecipe({ ...recipe, likeCount: newLikes });
+        }
+      });
+  };
 
   return (
     <div className="max-w-4xl mx-auto px-6 py-10 mt-10 mb-20 bg-white shadow-md rounded-xl">
@@ -22,6 +55,10 @@ const RecipeDetails = () => {
         alt={title}
         className="w-full h-64 object-cover rounded-lg mb-6"
       />
+
+      <h3 className="text-lg font-semibold text-pink-600 mb-4">
+        {likeCount} people interested in this recipe
+      </h3>
 
       <h1 className="text-3xl font-bold mb-2">{title}</h1>
       <p className="text-sm text-gray-500 mb-4">
@@ -49,7 +86,10 @@ const RecipeDetails = () => {
         </p>
       </div>
 
-      <button className="mt-6 bg-pink-600 hover:bg-pink-700 text-white px-6 py-2 rounded-md cursor-pointer">
+      <button
+        onClick={handleLike}
+        className="mt-6 bg-pink-600 hover:bg-pink-700 text-white px-6 py-2 rounded-md cursor-pointer"
+      >
         ❤️ Like
       </button>
     </div>
